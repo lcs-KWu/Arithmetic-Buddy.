0d959c81659c79d45030eeb107fb64d03779b797
Kevin Wu <KWu@lcs.on.ca>
2025-01-18 10:15:33 -0500

    Initial Commit

---
+++ Arithmetic Buddy./Arithmetic_Buddy_App.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_Buddy_App.swift
+//  Arithmetic Buddy.
+//
+//  Created by Yixuan Wu on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_Buddy_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
