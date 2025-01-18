f97d3648d60c1d1dd2dcdc9da45c246ec23400d0
Kevin Wu <KWu@lcs.on.ca>
2025-01-18 10:21:07 -0500

    added addition view

--- Arithmetic Buddy./Arithmetic_Buddy_App.swift
+++ Arithmetic Buddy./Arithmetic_Buddy_App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Arithmetic_Buddy_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            additionView()
         }
     }
 }
---
+++ Arithmetic Buddy./additionView.swift
@@ -0,0 +1,18 @@
+//
+//  additionView.swift
+//  Arithmetic Buddy.
+//
+//  Created by Yixuan Wu on 2025-01-18.
+//
+
+import SwiftUI
+
+struct additionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    additionView()
+}
